0c9221d5386cd94b7563f270e15d4d5b91c6a06d
jussmen <jussmen@yahoo.co.jp>
2025-06-26 23:22:52 +0900

    remove svg 2

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,4 @@
 import { useState, useEffect } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import { TodoApi } from './utils/todoApi'
 import type { Todo } from './types/Todo'
