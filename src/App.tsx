aa35d62dde73bf16de0db4168549e609fd81f28c
jussmen <jussmen@yahoo.co.jp>
2025-06-26 23:25:21 +0900

    remove vite and react

--- src/App.tsx
+++ src/App.tsx
@@ -240,19 +240,7 @@ function App() {
         boxShadow: '0 2px 16px rgba(0,0,0,0.04)',
         borderRadius: '8px',
       }}>
-        <div>
-          <a href="https://vite.dev" target="_blank">
-            <img src={viteLogo} className="logo" alt="Vite logo" />
-          </a>
-          <a href="https://react.dev" target="_blank">
-            <img src={reactLogo} className="logo react" alt="React logo" />
-          </a>
-        </div>
-        <h1>Vite + React</h1>
         {renderContent()}
-        <p className="read-the-docs">
-          Click on the Vite and React logos to learn more
-        </p>
       </div>
     </>
   )
